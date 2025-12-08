239207a6b9695143a71a1e534737b83955d61b0a
Josh <josh.t.richards@gmail.com>
2025-12-08 10:35:13 -0500

    test(TestCase): '.ocdata' -> '.ncdata' in knownEntries
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- tests/lib/TestCase.php
+++ tests/lib/TestCase.php
@@ -408,7 +408,7 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase {
 			'nextcloud.log' => true,
 			'audit.log' => true,
 			'owncloud.db' => true,
-			'.ocdata' => true,
+			'.ncdata' => true,
 			'..' => true,
 			'.' => true,
 		];
